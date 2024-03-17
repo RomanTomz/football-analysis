0caa73970621fc5f5effe28a28318d783931696d
Tommaso Di Marcello <t.dimarcello@gmail.com>
2024-03-17 21:59:55 +0000

    data collection moved to rpi

--- data_collection/weather_data.py
+++ data_collection/weather_data.py
@@ -99,18 +99,74 @@ def fetch_and_merge_weather_data(all_data_df, fetch_weather_data):
 
     return merged_df
 
-url = " https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/firenze/2019-01-01/2019-01-01?unitGroup=metric&key=ZMM2U9XUSJ6UV37L4L49NQACY&options=preview&contentType=json"
+# url = " https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{name}/{date}/{date}?unitGroup=metric&key=ZMM2U9XUSJ6UV37L4L49NQACY&options=preview&contentType=json"
 
-headers = {
-    'Accept': 'application/json',
-    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15',
-    "referer": "https://www.visualcrossing.com/weather-data"
-}
+# headers = {
+#     'Accept': 'application/json',
+#     'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15',
+#     "referer": "https://www.visualcrossing.com/weather-data"
+# }
 
-r = requests.get(url, headers=headers)
-# print(pd.DataFrame(r.json()))
-print(r.json())
+# r = requests.get(url, headers=headers)
+# print(r.json())
 
-# print(weather_data_df)
+def fetch_weather_data(name, date, api_key="ZMM2U9XUSJ6UV37L4L49NQACY"):
+    formatted_date = pd.to_datetime(date).strftime('%Y-%m-%d')
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{name}/{formatted_date}/{formatted_date}?unitGroup=metric&key={api_key}&options=preview&contentType=json"
 
-# all_data_df.to_csv("/Users/admin/git_projects/football/data_collection/serie_a_weather_2.csv", index=False)
+    headers = {
+        'Accept': 'application/json',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15',
+        "referer": "https://www.visualcrossing.com/weather-data" 
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        sleep(0.5)  # Respect the service's rate limit
+        data = response.json()
+        hours_data = data.get('days', [{}])[0].get('hours', [])
+        # print(hours_data)
+        midday_data = next((hour for hour in hours_data if hour['datetime'].endswith('13:00:00')), None)
+        if midday_data:
+            # Extract the relevant fields from the midday data
+            weather_data = {
+                'temp': midday_data.get('temp'),
+                'feelslike': midday_data.get('feelslike'),
+                'humidity': midday_data.get('humidity'),
+                'dew': midday_data.get('dew'),
+                'precip': midday_data.get('precip'),
+                'windspeed': midday_data.get('windspeed'),
+                'winddir': midday_data.get('winddir'),
+                'pressure': midday_data.get('pressure'),
+                'visibility': midday_data.get('visibility'),
+                'cloudcover': midday_data.get('cloudcover'),
+                'solarradiation': midday_data.get('solarradiation'),
+                'conditions': midday_data.get('conditions'),
+            }
+            return weather_data
+    return None
+
+
+def fetch_and_merge_weather_data(all_data_df, fetch_weather_data):
+    weather_data_list = []
+
+    for index, row in tqdm(all_data_df.iterrows(), total=all_data_df.shape[0]):
+        weather_data = fetch_weather_data(row['city_name'], row['Date'])  
+        if weather_data:
+            print(f"fetched weather data for {row['city_name']} on {row['Date']}")
+            weather_data['game_id'] = row['game_id']
+            print(weather_data)
+            weather_data_list.append(weather_data)
+        else:
+            print(f"weather data not found for {row['city_name']} on {row['Date']}")
+
+    # Convert the list of weather data to a DataFrame
+    weather_data_df = pd.DataFrame(weather_data_list)
+
+    # Merge the weather data with the original DataFrame
+    merged_df = pd.merge(all_data_df, weather_data_df, on='game_id')
+
+    return merged_df
+
+merged_df = fetch_and_merge_weather_data(all_data_df, fetch_weather_data)
+merged_df.to_csv("/Users/admin/git_projects/football/data_collection/serie_a_weather.csv", index=False)
