cfb43692680f07d9485a6e62c285ba7b1b204909
Tommaso Di Marcello <t.dimarcello@gmail.com>
2024-03-20 16:37:51 +0000

    Refactor one_to_one function and add MatchHistory class

--- processing/processing_utils.py
+++ processing/processing_utils.py
@@ -7,9 +7,11 @@ import pandas as pd
 
 from data_collection.data_reader import collect_data
 
-df = pd.read_csv("/Users/admin/git_projects/football/data_collection/serie_a_weather_2.csv")
 
-def one_to_one(df, home_team: str, away_team:str):
+df = pd.read_csv("/Users/admin/git_projects/football/data_collection/serie_a.csv")
+path = "/Users/admin/git_projects/football/data_collection/serie_a.csv"
+
+def one_to_one(df, home_team: str, away_team:str, total: bool = False):
     """
     Returns a DataFrame containing the one-to-one match history between two teams.
 
@@ -21,6 +23,52 @@ def one_to_one(df, home_team: str, away_team:str):
     Returns:
     DataFrame: A DataFrame containing the one-to-one match history between the two teams.
     """
-    return df.query(f"(HomeTeam == '{home_team}' and AwayTeam == '{away_team}')")
+    if total:
+        return df.query(f"(HomeTeam == '{home_team}' and AwayTeam == '{away_team}') or (HomeTeam == '{away_team}' and AwayTeam == '{home_team}')")
+    else:
+        return df.query(f"(HomeTeam == '{home_team}' and AwayTeam == '{away_team}')")
 
-print(one_to_one(df, "Juventus", "Inter"))
+# print(one_to_one(df, "Juventus", "Inter", total=True))
+
+class MatchHistory:
+    def __init__(self, data_path: str):
+        
+        self.df = pd.read_csv(data_path)
+    
+    def one_to_one(self, home_team: str, away_team:str, total: bool = False):
+        """
+        Returns a DataFrame containing the one-to-one match history between two teams.
+
+        Parameters:
+        home_team (str): The name of the home team.
+        away_team (str): The name of the away team.
+
+        Returns:
+        DataFrame: A DataFrame containing the one-to-one match history between the two teams.
+        """
+        if total:
+            return self.df.query(f"(HomeTeam == '{home_team}' and AwayTeam == '{away_team}') or (HomeTeam == '{away_team}' and AwayTeam == '{home_team}')")
+        else:
+            return self.df.query(f"(HomeTeam == '{home_team}' and AwayTeam == '{away_team}')")
+    
+    def match_stats(self, match_df):
+        """
+        Returns the match statistics for a given match DataFrame.
+
+        Parameters:
+        match_df (DataFrame): The DataFrame containing the match data.
+
+        Returns:
+        dict: A dictionary containing the match statistics.
+        """
+        stats = {
+            "home_goals": match_df["FTHG"].sum(),
+            "away_goals": match_df["FTAG"].sum(),
+        }
+        
+        return stats
+    
+history = MatchHistory(path)
+print(match_df := history.one_to_one("Juventus", "Inter", total=True))
+stats = history.match_stats(match_df)
+print(stats)
